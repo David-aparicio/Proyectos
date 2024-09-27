6586d6c5fa304dbb583e157c4c2b4ef8d7849ba9
COMPU-S036\daw1 <neiser.pintos21@gmail.com>
2024-09-27 09:15:49 +0200

    Actividad1

---
+++ Actividad1/src/com/decroly/daw/Actividad1.java
@@ -0,0 +1,151 @@
+package com.decroly.daw;
+
+import java.util.Scanner;
+
+import javax.annotation.processing.SupportedSourceVersion;
+
+public class Actividad1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+//	System.out.println("Buenos días");// Ejercicio 1
+	
+	// Ejercicio 2
+//	double lado = 5;
+		
+//	System.out.println("El area del cuadrado es:" + (lado * lado));
+
+	// Ejercicio 3
+	
+	Scanner reader = new Scanner(System.in);
+/*	System.out.println("Escriba el lado");
+	double lado1 = reader.nextDouble();
+	
+	System.out.println("El area es : " +  lado1 * lado1);
+	// Ejercicio 4
+	
+	reader = new Scanner(System.in);
+	System.out.println("Escriba el primer numero");
+	double x = reader.nextDouble();
+	
+	System.out.println("Escriba el segundo numero");
+	double y = reader.nextDouble();
+	
+	System.out.println("EL valor de la suma es " + (x + y));
+	System.out.println("El valor de la resta es " + (x - y));
+	System.out.println("El valor del producto " + (x % y));
+	System.out.println("El valor de la division " + (x / y));
+	
+	// Ejercicio 5
+	
+	reader = new Scanner(System.in);
+	System.out.println("Escriba un número que corresponda a la longitud del radio");
+	double radio = reader.nextDouble();
+	
+	double longitud = (2 * Math.PI) * radio;
+	double area = Math.PI * Math.pow(radio,2);
+	double volumen = (4.0 / 3.0) * Math.PI * Math.pow(radio, 3);
+	
+	
+	
+	System.out.println("La longitud de la circunferencia es: " + longitud);
+	System.out.println("El area de la circunferencia es: " + area);
+	System.out.println("El volumen de la circunferencia es: " + volumen);
+	
+	// Ejercicio 6
+	reader = new Scanner(System.in);
+	System.out.println("Escriba el precio del articulo");
+	double precioReal = reader.nextDouble();
+	
+	System.out.println("Escriba el precio de venta real");
+	double precioVenta = reader.nextDouble();
+	
+	double descuento = ((precioVenta - precioReal) / precioVenta) * 100;
+	
+	System.out.println("El porcentaje de descuento es: " + (descuento + "%"));
+	
+	// Ejercicio 7
+	
+	reader = new Scanner(System.in);
+	System.out.println("Escriba el numero en millas: ");
+	double millas = reader.nextDouble();
+	
+	double metros = millas * 1852;
+	
+	System.out.println("Son: " + (metros + "m"));
+	
+	// Ejercicio 8
+	
+	reader = new Scanner(System.in);
+	System.out.println("Escriba el primer número");
+	double Primernum = reader.nextDouble();
+	
+	System.out.println("Escriba el segundo número");
+	double Segundonum = reader.nextDouble();
+	
+	double mayor = Math.max(Primernum, Segundonum);
+	double menor = Math.min(Primernum, Segundonum);
+	
+	System.out.println("El orden de menor a mayor es: " + menor + " " + mayor);
+	
+*/	// Ejercicio 9
+	
+	reader = new Scanner(System.in);
+	System.out.println("Escriba el primer número");
+	double Primernum1 = reader.nextDouble();
+	
+	System.out.println("Escriba el segundo número");
+	double Segundonum1 = reader.nextDouble();
+	
+	double mayor1 = Math.max(Primernum1, Segundonum1);
+	boolean iguales = Primernum1 == Segundonum1;
+	
+	System.out.println(iguales + "Son iguales");
+	System.out.println("El numero mayor es: " + mayor1);
+	
+
+	
+	 //Ejercicio 10
+	 
+	 reader = new Scanner(System.in);
+	 System.out.println("Escriba un número");
+	 double x = reader.nextDouble();
+	 
+	 System.out.println("Escriba otro número");
+	 double y = reader.nextDouble();
+	 
+	 System.out.println("Escriba otro número");
+	 double z = reader.nextDouble();
+	 
+	 double Primera = Math.max(x, y);
+	 double definitiva = Math.max(Primera, z);
+	 
+	 System.out.println("El numero mayor es: " + definitiva);
+	 
+	 //Ejercicio 11
+	 
+	 reader = new Scanner(System.in);
+	 System.out.println("Escriba el primer numero");
+	 double 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
+	
