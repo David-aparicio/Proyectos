6599626423017b603121f2028d7971997527c0e8
COMPU-S036\daw1 <neiser.pintos21@gmail.com>
2024-10-24 08:56:25 +0200

    24/10//2024

---
+++ Actividad5/src/com/decroly/daw/Actividad5.java
@@ -0,0 +1,77 @@
+package com.decroly.daw;
+
+import java.util.Scanner;
+
+public class Actividad5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Ejercicio1
+		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("Escribe una cadena de texto: ");
+//		String cadena = sc.nextLine();
+//		
+//		String [] palabras = cadena.split(" ");
+//		for(String palabra : palabras) {
+//			System.out.println(palabra);
+//		}
+//		
+		//Ejercicio2
+		sc = new Scanner(System.in);
+		
+		System.out.println("Escriba una cadena de texto: ");
+		String cadena2 = sc.nextLine();
+		
+		System.out.println("Escriba una cadena de texto: ");
+		String cadena3 = sc.nextLine();
+		
+		if (cadena2.equalsIgnoreCase(cadena3)){
+			System.out.println("Son iguales");
+		}else {
+			
+			System.out.println("No son iguales");
+		}
+		// Ejercicio3
+		sc = new Scanner()
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
