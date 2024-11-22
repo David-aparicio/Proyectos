86e4c55151ebb3d1484ce8b43f45f327f6abc226
COMPU-S036\daw1 <neiser.pintos21@gmail.com>
2024-11-22 09:51:49 +0100

    22/11/2024

--- UD3 - DawBank/src/DawBank.java
+++ UD3 - DawBank/src/DawBank.java
@@ -1,14 +1,35 @@
-
-import java.io.Reader;
 import java.util.Scanner;
 
 public class DawBank {
         public static void main(String[] args) throws Exception {
             
-    String opcion = "";
+            Scanner reader  = new Scanner (System.in);
+            String opcion = "";
 
             do { 
-                Scanner reader  =
+                reader = new Scanner(System.in);
+                System.out.println("Por favor seleccione una opcion \n" + "1) Datos de la cuenta. \n" + "2) IBAN. \n" + "3) Titular.\n" +"4) Saldo. \n" + "5) Ingreso. \n" + "6) Retirada. \n" + "7) Movimiento. \n" + "8) Salir. \n");
+                opcion = reader.nextLine();
+
+                    switch(opcion){
+                            
+                        case "1":
+
+
+
+
+                            
+                    }
+
+
+
+
+
+
+
+
+                opcion = reader.nextLine();
+
                 
             } while (true);
     }
