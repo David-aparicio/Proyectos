8e9cdfdc2f49e1eb1f33053d38090370ebea2395
COMPU-S036\daw1 <neiser.pintos21@gmail.com>
2024-12-02 12:49:39 +0100

    02/12/2024

---
+++ VideoDaw/src/Genero.java
@@ -0,0 +1,3 @@
+public enum Genero {
+    Accion, Aventura, Ciencia_ficcion, Comedia, Documentales, Drama, Fantasia, Musical;
+}
--- VideoDaw/src/Pelicula.java
+++ VideoDaw/src/Pelicula.java
@@ -1,5 +1,35 @@
-public class Pelicula {
-    
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Pelicula {
+     
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    private static int contador = 0;
+
+    private int codigoPelicula;
+    private String titulo;
+    private Genero genero;
+    private LocalDate fechaRegistro;
+    private LocalDate fechaBaja;
+    private LocalDateTime fechaAlquilada;
+    private boolean IsAlquilada;
+
+
+    public Pelicula(String CodigoPelicula,Genero genero, String Titulo){
+       this.codigoPelicula = contador;
+       contador++;
+       this.titulo = titulo;
+       this.genero = genero;
+       this.fechaRegistro = LocalDate.now();
+       this.fechaBaja = LocalDate.now();
+       this.fechaAlquilada = LocalDateTime.now();
+       this.IsAlquilada = false;
+    }
+
+    public int getCodigoPelicula(){
+        return codigoPelicula;
+    }
 
 }
