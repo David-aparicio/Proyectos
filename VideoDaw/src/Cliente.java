f74c2490037c84d57b130ecaec4ccfdd225e2141
COMPU-S036\daw1 <neiser.pintos21@gmail.com>
2024-12-04 10:10:10 +0100

    04/12/2024

--- VideoDaw/src/Cliente.java
+++ VideoDaw/src/Cliente.java
@@ -1,4 +1,17 @@
+
+import java.time.LocalDate;
+
 public class Cliente {
-    private String DNI;
-    
+
+
+    private String dni;
+    private String nombre;
+    private int numSocio;
+    private String direccion;
+    private LocalDate fechaNacimiento;
+    private LocalDate fechaBaja;
+    private Pelicula [] peliculasAlquiladas;
+
+    public Cliente(String dni, )
+
 }
--- VideoDaw/src/Pelicula.java
+++ VideoDaw/src/Pelicula.java
@@ -8,28 +8,53 @@ public class Pelicula {
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     private static int contador = 0;
 
-    private int codigoPelicula;
+    private String codigoPelicula;
     private String titulo;
-    private Genero genero;
+    private Genero pGenero;
     private LocalDate fechaRegistro;
     private LocalDate fechaBaja;
     private LocalDateTime fechaAlquilada;
-    private boolean IsAlquilada;
+    private boolean isAlquilada;
 
 
-    public Pelicula(String CodigoPelicula,Genero genero, String Titulo){
-       this.codigoPelicula = contador;
+    public Pelicula(String codigoPelicula,Genero genero, String titulo){
+
+       this.codigoPelicula = "PE-"+contador;
        contador++;
        this.titulo = titulo;
-       this.genero = genero;
+       this.pGenero = pGenero;
        this.fechaRegistro = LocalDate.now();
        this.fechaBaja = LocalDate.now();
        this.fechaAlquilada = LocalDateTime.now();
-       this.IsAlquilada = false;
+       this.isAlquilada = false;
     }
 
-    public int getCodigoPelicula(){
+    public String getCodigoPelicula(){
         return codigoPelicula;
+    }
+    public String getTitulo(){
+        return titulo;
+    }
+    public Genero getPGenero(){
+        return pGenero;
+    }
+    public LocalDate getFechaRegistro(){
+        return fechaRegistro;
+    }
+    public LocalDate getFechaBaja(){
+        return fechaBaja;
+    }
+    public LocalDateTime getFechaAlquilada(){
+        return fechaAlquilada;
+    }
+    public boolean getIsAlquilada(){
+        return isAlquilada;
+    }
+
+    public String mostrarInfoPelicula(){
+        String info = String.format("Pelicula - codigoPelicula: %s titulo: %s pGenero: %s fechaRegistro: %s fechaBaja: %s fechaAlquilada: %s isAlquilada: ", this.codigoPelicula, this.titulo, this.pGenero, this.fechaRegistro,this.fechaBaja
+        , this.fechaAlquilada,this.isAlquilada);
+        return info;
     }
 
 }
