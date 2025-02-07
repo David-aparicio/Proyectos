1314fe4f19d4347e3bf6d293603adf1a8b66cb79
COMPU-S036\daw1 <neiser.pintos21@gmail.com>
2025-02-07 09:50:06 +0100

    Joel no me copies el github

--- Colecciones y Estructuras de datos/src/App.java
+++ Colecciones y Estructuras de datos/src/App.java
@@ -1,5 +1,4 @@
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -34,21 +33,61 @@ public class App {
         }catch (ArithmeticException e) {
             System.out.println("Error en la operacion");
         }
-*/
-        //Ejercicio3
-        double [] vector = new double[5];
-        try{
-        for(int i = 0; i < vector.length; i++){
-            System.out.println("Introduce el valor del número " );
-            vector[i] = sc.nextDouble();
-        }
-        for(int i = 0; i < vector.length; i++){
-            System.out.println("El valor del numero es: "+ vector[i]);
-        }
-    }catch(InputMismatchException e){
-        System.out.println("El valor es incorrecto");
-    }
-    //Ejercicio4
 
-}
+        //Ejercicio3
+        
+        double [] vector = new double[5];
+
+        for(int i = 0; i < vector.length; i++){
+            
+            try{
+                sc = new  Scanner(System.in);
+                System.out.println("Introduce el valor del número " );
+                vector[i] = sc.nextDouble();
+                
+            
+        }catch(InputMismatchException e){
+            System.out.println("El valor es incorrecto");
+            
+        }
+    }
+    */
+    
+    //Ejercicio4
+            sc = new  Scanner(System.in);
+            boolean continuar = true;
+    
+            while (continuar) {
+                try {
+                    System.out.print("Introduce un número: ");
+                    int numero = sc.nextInt();
+    
+                    
+                    imprimePositivo(numero);
+    
+                    imprimeNegativo(numero);
+    
+                } catch (Exception e) {
+                    System.out.println("Valor incorrecto");
+                } finally {
+                    System.out.print("¿Quieres introducir otro número? (s/n): ");
+                    String respuesta = sc.next();
+                    if (!respuesta.equalsIgnoreCase("s")) {
+                        continuar = false;
+                    }
+                }
+            }
+        }
+        public static void imprimePositivo(int p) throws Exception {
+            if (p < 0) {
+                throw new Exception("Error: El número debe ser positivo.");
+            }
+        }
+    
+        public static void imprimeNegativo(int n) throws Exception {
+            
+            if (n >= 0) {
+                throw new Exception("Error: El número debe ser negativo.");
+            }
+        }
 }
