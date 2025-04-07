52a2e4862eb3ec59d508edc2948e9bd92c67c715
David-aparicio <neiser.pintos21@gmail.com>
2025-04-07 19:52:44 +0200

    Terminado
    terminado

--- UD8_act1/unidad8_act1/src/main/java/org/example/SQLDataBaseManager.java
+++ UD8_act1/unidad8_act1/src/main/java/org/example/SQLDataBaseManager.java
@@ -1,22 +1,44 @@
 package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SQLDataBaseManager {
+public class SQLDataBaseManager{
 
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/";
-    private static final String SCHEMA = "unidad8act1";
-    private static final String USUARIO = "root";
-    private static final String CLAVE = "daw12";
+    private static String DRIVER;
+    private static String URL;
+    private static String SCHEMA;
+    private static String USUARIO;
+    private static String CLAVE;
 
-    public static Connection getConnection() {
+    public static Connection getConnection(){
         Connection connection = null;
+
+        try(FileReader fichero = new FileReader("Recursos\\aplicacion");
+            BufferedReader lector = new BufferedReader(fichero)) {
+            String linea = lector.readLine();
+            while (linea != null) {
+                String[] credenciales = linea.split(",");
+                DRIVER = credenciales[0];
+                URL = credenciales[1];
+                SCHEMA = credenciales[2];
+                USUARIO = credenciales[3];
+                CLAVE = credenciales[4];
+
+                linea = lector.readLine();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error al leer el fichero de credenciales: " + e.getMessage());
+        }
 
         try {
             Class.forName(DRIVER);
-            connection = DriverManager.getConnection(URL + SCHEMA, USUARIO, CLAVE);
+            connection = DriverManager.getConnection(URL+SCHEMA, USUARIO, CLAVE);
+
         } catch (ClassNotFoundException e) {
             System.out.println("Error de acceso al driver: " + e.getMessage());
         } catch (SQLException e) {
@@ -25,4 +47,5 @@ public class SQLDataBaseManager {
 
         return connection;
     }
+
 }
